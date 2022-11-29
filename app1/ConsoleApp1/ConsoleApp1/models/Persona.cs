298f6b07f8f944b7cfe7c0547aa668fe18379891
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-11-29 17:34:42 -0300

    primeras cosas

---
+++ app1/ConsoleApp1/ConsoleApp1/models/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1.models;
+
+public class Persona
+{
+    public int Id { get; set; }
+
+    public String Nombre { get; set; }
+
+    public DateTime FechaNacimiento { get; set; }
+}
